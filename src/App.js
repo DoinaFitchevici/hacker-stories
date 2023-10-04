9b217b7072adf4d61d312aa56da3d6e05513d4ed
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-10-04 08:16:46 -0400

    More refactoring

--- src/App.js
+++ src/App.js
@@ -19,22 +19,17 @@ const list = [
   },
 ];
 
-const List = () => (
-  <ul>
-    {list.map(function (item) {
-      return (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      );
-    })}
-  </ul>
-);
+const List = () =>
+  list.map((item) => (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  ));
 
 const Search = () => (
   <div>
