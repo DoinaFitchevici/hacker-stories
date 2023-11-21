2db094953eee727a3749e9f4ec4720bd64c683be
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-11-21 05:10:47 -0500

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -91,6 +91,22 @@ const App = () => {
     },
   ];
 
+  const SET_STORIES = "SET_STORIES";
+  const REMOVE_STORY = "REMOVE_STORY";
+
+  const storiesReducer = (state, action) => {
+    switch (action.type) {
+      case SET_STORIES:
+        return action.payload;
+      case REMOVE_STORY:
+        return state.filter(
+          (story) => action.payload.objectID !== story.objectID
+        );
+      default:
+        throw new Error();
+    }
+  };
+
   const getAsyncStories = () =>
     new Promise((resolve) =>
       setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
@@ -98,7 +114,7 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -107,18 +123,20 @@ const App = () => {
 
     getAsyncStories()
       .then((result) => {
-        setStories(result.data.stories);
+        dispatchStories({
+          type: SET_STORIES,
+          payload: result.data.stories,
+        });
         setIsLoading(false);
       })
       .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({
+      type: REMOVE_STORY,
+      payload: item,
+    });
   };
 
   const handleSearch = (event) => {
